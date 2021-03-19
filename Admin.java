9cc94ae860bec971b579bb19ab278d3f12fe87ef
Fiona-Vong <56323594+Fiona-Vong@users.noreply.github.com>
2021-03-19 13:55:42 +0800

    Implemented Admin.java

--- Admin.java
+++ Admin.java
@@ -1,3 +1,45 @@
-public class Admin {
+import java.util.ArrayList;
 
+public class Admin implements User{
+    private String userName;
+    private String type;
+    private double credit;
+    private ArrayList<String> gameOwned;
+
+    public Admin(String username, String type, double credit, ArrayList <String> gameOwned){
+        this.userName = username;
+        this.type = type;
+        this.credit = credit;
+        this.gameOwned = gameOwned;
+
+    }
+    @Override
+    public String getUserName(){
+        return this.userName;
+    }
+
+    @Override
+    public String getType(){
+        return this.type;
+    }
+
+    @Override
+    public double getCredit(){
+        return this.credit;
+    }
+
+    @Override
+    public ArrayList <String> getOwnedGame(){
+        return this.gameOwned;
+    }
+
+    @Override
+    public void addCredit(double credit){
+        this.credit+=credit;
+    }
+
+    @Override
+    public void addOwnedGame(String game){
+        this.gameOwned.add(game);
+    }
 }
