ce0812d13eac3a6fa0752182573901c5c97f6579
Fiona-Vong <56323594+Fiona-Vong@users.noreply.github.com>
2021-03-19 13:38:31 +0800

    Finished task 4.4: update() to update information in their corresponding file

---
+++ data_base.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class data_base {
+    public static String userData = "userName.txt";
+    public static String dailyData = "daily.txt";
+
+    public static void update(String data, String filePath){
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
+            writer.write(data);
+            writer.newLine();
+            writer.flush();
+            writer.close();
+            System.out.println(filePath);
+        }catch(IOException e){
+            System.out.println("ERROR: "+e.getMessage());
+        }
+    }
+
+}
+
+
