7e13aa46e44c788d6a98e96d1b783053f0027592
Fiona-Vong <56323594+Fiona-Vong@users.noreply.github.com>
2021-03-19 13:58:26 +0800

    Completed task 4.5: Finsihed ifUserExist(String userName) to check the existence of a user name

--- data_base.java
+++ data_base.java
@@ -1,13 +1,10 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class data_base {
     public static String userData = "userName.txt";
     public static String dailyData = "daily.txt";
 
     public static void appendData(String data, String filePath){
-
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
             writer.write(data);
@@ -19,6 +16,22 @@ public class data_base {
             System.out.println("ERROR: "+e.getMessage());
         }
     }
+
+    public static boolean ifUserExist (String userName, String filePath) throws IOException {
+        File inputFile = new File(filePath);
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        String currentLine;
+        while((currentLine = reader.readLine()) != null) {
+            String[] tokens = currentLine.split(" ");
+            if(tokens[0].equals(userName)){
+                return true;
+            }
+        }
+        reader.close();
+        return false;
+    }
+    
+
 
 }
 
