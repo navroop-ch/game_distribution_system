63ce12b6db96513e115dfc8dc5593992488f73c2
Hadi hafeez <hadi@Hadis-MacBook-Pro.local>
2021-03-27 08:50:05 +0500

    Created Seller class

---
+++ Seller.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Seller extends User{
+
+    protected Seller(String username, double credit, ArrayList <String> gameOwned){
+        super(username, credit, gameOwned);
+        this.type = SELLER_USER_TYPE;
+
+    }
+
+    @Override
+    protected void buy(String title, String sellerName){
+        //Todo: Return warning via exception
+        System.out.println("Your user type cannot buy games!");
+    }
+}
