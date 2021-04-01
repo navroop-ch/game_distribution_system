b711d8534b527bec13433ff1968f684f1b1aa3a9
Hadi hafeez <hadi@Hadis-MacBook-Pro.local>
2021-04-01 09:43:04 +0500

    Added key-authentication for session to data_base instance exchange and fixed minute singleton issue

--- Session.java
+++ Session.java
@@ -1,4 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.security.SecureRandom;
+import java.util.Arrays;
 
 // A session object that controls exchange of data between User objects and the database. A session must be logged into
 // for a user to get access to the database to perform any transactions.
@@ -7,35 +10,52 @@ public class Session {
 
     private static Session instance = null; // For singleton
 
-    private final data_base dataBase;
+    private static byte[] dataBaseKey;
+    private static SecureRandom random = new SecureRandom();
+    private static data_base dataBase;
     private ArrayList<User> userList;
     private User userLoggedIn = null;
     private boolean loginStatus;
 
-    private Session(data_base db){
-        this.dataBase = db;
+    private Session(){
         // Todo: load users into userList by creating a method for it in data_base.java
-
     }
 
+    private static void generateKey(){
+        dataBaseKey = random.generateSeed(10);
+    }
+
+    protected static boolean authenticateKey(byte[] key){
+        return Arrays.equals(key, dataBaseKey);
+    }
+
+
+    private static void setDataBase(data_base db){
+        dataBase = db;
+    }
     // Singleton implementation
     protected static Session getInstance() {
-        // lazy initialization
         if (instance == null) {
-            data_base dataBase = new data_base();
-            instance = new Session(dataBase);
+            instance = new Session();
+
+            // Authentication: generates a new key, stores it, and then passes it for authentication
+            generateKey();
+            data_base dataBase = data_base.getInstance(dataBaseKey);
+            setDataBase(dataBase);
         }
         return instance;
     }
 
     protected data_base getDataBase(User user){
-        if (this.userLoggedIn == user){ return this.dataBase;}
+        if (this.userLoggedIn == user){ return dataBase;}
         else {return null;}
     }
 
     protected void sessionLogin(User user){
-        this.userLoggedIn = user;
-        this.loginStatus = true;
+        if(this.userLoggedIn == null) {
+            this.userLoggedIn = user;
+            this.loginStatus = true;
+        }
     }
 
     protected void sessionLogout(User user){
