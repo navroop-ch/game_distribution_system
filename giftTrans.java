ceef16a23663495d5e0c23c94d0d845bef06bbcc
Fiona-Vong <56323594+Fiona-Vong@users.noreply.github.com>
2021-04-04 01:55:20 +0800

    Fixing gift feature

--- giftTrans.java
+++ giftTrans.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 public class giftTrans extends Transaction{
-    protected User gameOwner;
-    protected User gameReceiver;
-    protected Game gameGift;
+    protected String gameOwner;
+    protected String gameReceiver;
+    protected String gameGift;
 
-    protected giftTrans(String code, Game giftGame, User gameReceiver, User gameOwner){
+    protected giftTrans(String code, String giftGame, String gameReceiver, String gameOwner){
         super(Session.getInstance().getLoggedInUserName());
         this.gameOwner= gameOwner;
         this.gameReceiver=gameReceiver;
@@ -14,15 +14,33 @@ public class giftTrans extends Transaction{
 
     }
     @Override
-    protected Boolean execute() {
-        if(gameGift.equals(gameOwner.owned(gameGift.getTitle()))) {
-            gameOwner.removeGame(gameGift);
-            gameReceiver.addOwnedGame(gameGift);
+    protected Boolean execute(Session session) {
+        User owner = session.getUser(this.gameOwner);
+        User receiver = session.getUser(this.gameReceiver);
+        if (owner != null && receiver!= null){
+
+            return true;
+        }
+        else {
+            //Todo: figure out appropriate error return
+            System.out.println("Error: User does not exist");
+            return false;
+        }
+        if(gameGift.equals(owner.owned(gameGift).getTitle())) {
+            Game game = owner.owned(gameGift);
+            owner.removeGame(game);
+            receiver.addOwnedGame(game);
             return true;
         }
 
         return false;
     }
+
+    //check format and correct trans code
+    protected Boolean transactionValidate(String code, String type, String credit) {
+        return typeValidation(type) && creditValidation(credit) && code==data_base.giftCode;
+    }
+
 
 
 }
