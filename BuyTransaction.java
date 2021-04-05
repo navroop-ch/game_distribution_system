ca2a4d3a60b7f3082b51526ba463151e70f38384
Your Name <navroopchandwani@gmail.com>
2021-04-05 07:13:17 +0530

    Added doc to BuyTransaction

--- BuyTransaction.java
+++ BuyTransaction.java
@@ -1,5 +1,7 @@
 public class BuyTransaction extends Transaction{
-
+    /**
+     * A Buy transaction class for buying a game from a seller
+     */
     protected String sellerUsername;
     protected String gameTitle;
 
@@ -13,14 +15,31 @@ public class BuyTransaction extends Transaction{
         else this.validTransaction = false;
     }
 
+    /**
+     * Checks the string for valid buy transaction code
+     * @param code string to be checked
+     * @return true if the string matches, false otherwise
+     */
     private Boolean buyCodeValidation(String code){
         return code.equals(data_base.buyCode) && codeValidation(code);
     }
 
+    /**
+     * Checks the user code, seller username and game's name for constraints
+     * @param code user code
+     * @param sellerUsername seller's username
+     * @param gameTitle game's name
+     * @return true if code, sellerUsername and gameTitle fulfill the constraints, false otherwise
+     */
     protected Boolean transactionValidate(String code, String sellerUsername, String gameTitle){
         return usernameValidation(sellerUsername) && buyCodeValidation(code) && gameTitleValidation(gameTitle);
     }
 
+    /**
+     * Executes buy transaction for a user
+     * @param session session objects that stores all the logged in users
+     * @return true if buy transaction is executed, false otherwise
+     */
     @Override
     protected Boolean execute(Session session) {
         User user = session.getUser(this.transactionUsername);
