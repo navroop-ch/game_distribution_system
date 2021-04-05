78cf5729a1ab82b492167fdc3b6aadf426bb061f
Your Name <navroopchandwani@gmail.com>
2021-04-05 05:52:31 +0530

    Added documentation to transaction.java

--- Transaction.java
+++ Transaction.java
@@ -15,8 +15,10 @@ public abstract class Transaction{
             User.ADMIN_USER_TYPE, User.BUYER_USER_TYPE, User.FULL_USER_TYPE, User.SELLER_USER_TYPE);
 
 
-    // reading daily.txt
-
+    /**
+     * Checks if a string username is valid
+     * @param username username to check
+     */
     protected Transaction(String username){
         // checks code length and username length
         if (usernameValidation(username)){
@@ -29,35 +31,75 @@ public abstract class Transaction{
     protected abstract Boolean execute(Session session);
 
 
+    /**
+     * Checks the length of the username entered
+     * @param username string to be checked
+     * @return true if this username's length matches the length stored in database (15)
+     */
     protected boolean usernameValidation(String username){
         return username.length() == data_base.USERNAME_LENGTH;
     }
 
+    /**
+     * Validates the code by checking its length and type
+     * @param code string to be checked
+     * @return true if the length of code matches the CODE_LENGTH in data_base and code is numeric
+     */
     protected boolean codeValidation(String code){
         return code.length() == data_base.CODE_LENGTH && integerValidation(code);
     }
 
+    /**
+     * Validates the type of user
+     * @param type the type of user (admin, seller std, buyer std, full-std)
+     * @return true if type's length matches the TYPE_LENGTH (2) and it exists in the list storing user types.
+     */
     protected boolean typeValidation(String type){
         // Check length and valid type
         return type.length() == data_base.TYPE_LENGTH && TYPES.contains(type) ;
     }
 
+    /**
+     * Validates the credit amount
+     * @param credit the amount string to be checked
+     * @return true if credit's length is 9 and its a doublem otherwise false
+     */
     protected boolean creditValidation(String credit){
         return credit.length() == data_base.CREDIT_LENGTH && doubleValidation(credit);
     }
 
+    /**
+     * Validates the discount string
+     * @param discount string to be checked
+     * @return true if discount's length matches DISCOUNT_LENGTH (5) and its a double, otherwise false
+     */
     protected boolean discountValidation(String discount){
         return discount.length() == data_base.DISCOUNT_LENGTH && doubleValidation(discount);
     }
 
+    /**
+     * Validates a game's name or title
+     * @param title game's name
+     * @return true if title's length matches TITLE_LENGTH (25), otherwise false
+     */
     protected boolean gameTitleValidation(String title){
         return title.length() == data_base.TITLE_LENGTH;
     }
 
+    /**
+     * Validates the game's price string
+     * @param price game's price
+     * @return true if price's length is PRICE_LENGTH (6) and its a double
+     */
     protected boolean salePriceValidation(String price){
         return price.length() == data_base.PRICE_LENGTH && doubleValidation(price);
     }
 
+    /**
+     * Validates a string of integers
+     * @param strInt an integer string
+     * @return true if the string is made of integers, false otherwise
+     */
     protected boolean integerValidation(String strInt){
         try {
             Integer.parseInt(strInt);
@@ -67,6 +109,11 @@ public abstract class Transaction{
         return true;
     }
 
+    /**
+     * Validates a string of double
+     * @param strD a string containing double
+     * @return true if string is made of double, false otherwise
+     */
     protected boolean doubleValidation(String strD){
         try {
             Double.parseDouble(strD);
