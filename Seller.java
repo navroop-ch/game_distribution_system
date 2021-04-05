0405b7c0682a2aa3cfa69c82f063e9b8d483f665
Your Name <navroopchandwani@gmail.com>
2021-04-05 08:24:14 +0530

    added javadoc to seller class

--- Seller.java
+++ Seller.java
@@ -1,14 +1,21 @@
 import java.util.ArrayList;
 
+/**
+ * A user of type sell standard with username, credit and an array of games owned.
+ */
 public class Seller extends User{
 
     protected Seller(String username, double credit, ArrayList<Game> gameOwned){
         super(username, SELLER_USER_TYPE, credit, gameOwned);
     }
 
+    /**
+     * A sell-standard user can't buy a game
+     * @param title The title of the game.
+     * @param sellerName The username of the seller.
+     */
     @Override
     protected void buy(String title, String sellerName){
-        //Todo: Return warning via exception
         System.out.println("Your user type cannot buy games!");
     }
 }
