dd759067bfe4ad2ca58228d2e6417678e0ffb2d8
Your Name <navroopchandwani@gmail.com>
2021-04-05 07:30:27 +0530

    added javadoc to fullstandarduser class

--- FullStandardUser.java
+++ FullStandardUser.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+/**
+ * A class for user of type full standard that has username, credit and an array of games owned
+ */
 public class FullStandardUser extends User{
 
     protected FullStandardUser(String username, double credit, ArrayList<Game> gameOwned){
