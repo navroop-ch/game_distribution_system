76e7ca1986c737732e8f99ece85ddb79749f7b66
Your Name <navroopchandwani@gmail.com>
2021-04-05 08:30:42 +0530

    added javadoc to sell transaction class

--- SellTransaction.java
+++ SellTransaction.java
@@ -1,9 +1,20 @@
+/**
+ * A transaction class for selling a game
+ */
 public class SellTransaction extends Transaction{
 
     protected String gameTitle;
     protected Double discount;
     protected Double salePrice;
 
+    /**
+     * Initialising the fields of sell transaction
+     * @param code transasction code
+     * @param gameTitle title/ name of the game
+     * @param username username of the game's owner
+     * @param discount discount on the game
+     * @param sale sale price
+     */
     protected SellTransaction(String code, String gameTitle, String username, String discount, String sale) {
         super(username);
         if (transactionValidate(username, code, gameTitle, discount, sale)){
@@ -16,15 +27,35 @@ public class SellTransaction extends Transaction{
         else this.validTransaction = false;
     }
 
+    /**
+     * Returns whether a transaction code is valid or not
+     * @param code transaction code
+     * @return true if the transaction code matches the sell code and is valid, flase otherwise
+     */
     private Boolean sellCodeValidation(String code){
         return code.equals(data_base.sellCode) && codeValidation(code);
     }
 
+    /**
+     * Returns whether a given transaction is a valid sell transaction
+     * @param username usename of the game's owner
+     * @param code transaction code
+     * @param gameTitle game's name
+     * @param discount discount on the game
+     * @param sale sale price of the game
+     * @return true if all the parameter are valid and follow the format constraints of a sell transaction, false
+     * otherwise
+     */
     private boolean transactionValidate(String username, String code, String gameTitle, String discount, String sale){
         return sellCodeValidation(code) && gameTitleValidation(gameTitle)
                 && discountValidation(discount) && salePriceValidation(sale);
     }
 
+    /**
+     * Sells a game to a user
+     * @param session session object that keeps track of all logged in users
+     * @return true if a game was sold to a user, false otherwise
+     */
     @Override
     protected Boolean execute(Session session) {
         User user = session.getUser(this.transactionUsername);
@@ -35,7 +66,7 @@ public class SellTransaction extends Transaction{
             return true;
         }
         else {
-            //System.out.println("Error: The user does not exist");
+            System.out.println("Error: The user does not exist. Sell transaction Failed !");
             return false;
         }
 
